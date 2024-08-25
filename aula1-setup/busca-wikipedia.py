a63ea5fa5eb4a3aa29645e94f180fe2848ae7a05
Lucas Nunes Batista <lucasnunesb6@gmail.com>
2024-08-25 13:27:17 -0400

    feat: adiciona funcionalidade de pesquisa com ferramenta Youtube ao busca-wikipedia.py

--- aula1-setup/busca-wikipedia.py
+++ aula1-setup/busca-wikipedia.py
@@ -1,6 +1,11 @@
 from langchain_community.tools import WikipediaQueryRun
 from langchain_community.utilities import WikipediaAPIWrapper
+from langchain_community.tools import YouTubeSearchTool
+
+youtube = YouTubeSearchTool()
 
 wikipedia = WikipediaQueryRun(api_wrapper=WikipediaAPIWrapper())
 
-print(wikipedia.run("Saint Seiya"))
+print(wikipedia.run("Aurora (singer)"))
+
+print(youtube.run("The Seed Song Haik Concert from Aurora singer"))
